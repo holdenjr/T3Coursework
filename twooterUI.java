89b1dd258518b031488c21d43dfe343ba4cafaf7
Holden <holdenjr@egb070000015.lancs.local>
2019-05-18 19:56:55 +0100

    Created the basic login screen for Twooter.

--- twooterUI.java
+++ twooterUI.java
@@ -2,21 +2,47 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class twooter
+public class twooterUI extends JFrame 
 {
-    public twooter()
+    JLabel label1, usrLabel, pswLabel;
+    JTextField tf1;
+    JButton btn1;
+    JPasswordField p1;
+
+    public twooterUI()
     {
-        JFrame board = new JFrame("Twooter");
-        JPanel panel = new JPanel();
-
-        GridLayout grid = new GridLayout(4,4);
-
-        board.setSize(400,400);
-        panel.setSize(400,400);
-        panel.setLayout(grid);
-
-        board.setTitle("Twooter");
-        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new JFrame("Twooter Login");
+        label1 = new JLabel("Login Form");
+        label1.setForeground(Color.blue);
+        label1.setFont(new Font("Serif", Font.BOLD, 20));
+ 
+        usrLabel = new JLabel("Username");
+        pswLabel = new JLabel("Password");
+        tf1 = new JTextField();
+        p1 = new JPasswordField();
+        btn1 = new JButton("Login");
+ 
+        label1.setBounds(100, 30, 400, 30);
+        usrLabel.setBounds(80, 70, 200, 30);
+        pswLabel.setBounds(80, 110, 200, 30);
+        tf1.setBounds(300, 70, 200, 30);
+        p1.setBounds(300, 110, 200, 30);
+        btn1.setBounds(150, 160, 100, 30);
+ 
+        frame.add(label1);
+        frame.add(usrLabel);
+        frame.add(tf1);
+        frame.add(pswLabel);
+        frame.add(p1);
+        frame.add(btn1);
+ 
+        frame.setSize(600, 400);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 
+    public static void main(String[] args)
+    {
+        new twooterUI();
+    }
 }
